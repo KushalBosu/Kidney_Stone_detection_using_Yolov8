c2553cf35a61168e787e6dadf4813c01b166b9af
KushalBosu <kushalbasu1421998@gmail.com>
2023-05-17 01:31:56 +0530

    modifies web app

--- app.py
+++ app.py
@@ -14,18 +14,21 @@ def index():
 def detect():
     """
     Handler of /detect POST endpoint
-    Receives uploaded file with a name "image_file", 
-    passes it through YOLOv8 object detection 
-    network and returns an array of bounding boxes.
-    :return: a JSON array of objects bounding 
-    boxes in format 
-    [[x1,y1,x2,y2,object_type,probability],..]
+    Receives uploaded file with a name "image_file",
+    passes it through YOLOv8 object detection
+    network and returns an array of bounding boxes
+    and kidney stone detection status.
+    :return: a JSON object with "boxes" and "kidney_stones_detected" keys
     """
     buf = request.files["image_file"]
-    boxes = detect_objects_on_image(Image.open(buf.stream))
+    boxes, kidney_stones_detected = detect_objects_on_image(Image.open(buf.stream))
+    response_data = {
+        "boxes": boxes,
+        "kidney_stones_detected": kidney_stones_detected
+    }
     return Response(
-      json.dumps(boxes),  
-      mimetype='application/json'
+        json.dumps(response_data),
+        mimetype='application/json'
     )
 
 
@@ -34,25 +37,28 @@ def detect_objects_on_image(buf):
     Function receives an image,
     passes it through YOLOv8 neural network
     and returns an array of detected objects
-    and their bounding boxes
+    and their bounding boxes, along with the kidney stone detection status.
     :param buf: Input image file stream
-    :return: Array of bounding boxes in format 
-    [[x1,y1,x2,y2,object_type,probability],..]
+    :return: Array of bounding boxes in format
+    [[x1,y1,x2,y2,object_type,probability],..], kidney_stones_detected (bool)
     """
     model = YOLO("best.pt")
     results = model.predict(buf)
     result = results[0]
     output = []
+    kidney_stones_detected = False
     for box in result.boxes:
         x1, y1, x2, y2 = [
-          round(x) for x in box.xyxy[0].tolist()
+            round(x) for x in box.xyxy[0].tolist()
         ]
         class_id = box.cls[0].item()
         prob = round(box.conf[0].item(), 2)
         output.append([
-          x1, y1, x2, y2, result.names[class_id], prob
+            x1, y1, x2, y2, result.names[class_id], prob
         ])
-    return output
+        if result.names[class_id] == "KIDNEY_STONE":
+            kidney_stones_detected = True
+    return output, kidney_stones_detected
 
 if __name__ == '__main__':
     app.run()
